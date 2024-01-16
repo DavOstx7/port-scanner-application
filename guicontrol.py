a19f24a767ebb6ad717c13b691795dcf6ca4980d
dav ost <private>
2024-01-16 12:08:12 +0200

    add GUI control logic for customized GUI elements

---
+++ guicontrol.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+
+# imports #
+from functools import partial
+from tkinter import *
+import sys
+import logging
+import os
+import subprocess
+from PIL import Image, ImageTk
+from tkinter import messagebox
+import time
+from datetime import datetime
+import threading
+import re
+from googlesearch import search
+
+
+# ~~~~~~~~#
+
+class HyperlinkManager:
+    """This class helps making a link to use in a textbox"""
+
+    def __init__(self, text):
+        self.text = text
+        self.text.tag_config("hyper", foreground="blue", underline=1)
+        self.text.tag_bind("hyper", "<Enter>", self._enter)
+        self.text.tag_bind("hyper", "<Leave>", self._leave)
+        self.text.tag_bind("hyper", "<Button-1>", self._click)
+        self.reset()
+
+    def reset(self):
+        self.links = {}
+
+    def add(self, action):
+        """ add an action to the manager.  returns tags to use in
+         associated text widget """
+
+        tag = "hyper-%d" % len(self.links)
+        self.links[tag] = action
+        return "hyper", tag
+
+    def _enter(self, event):
+        self.text.config(cursor="hand2")
+
+    def _leave(self, event):
+        self.text.config(cursor="")
+
+    def _click(self, event):
+        for tag in self.text.tag_names(CURRENT):
+            if tag[:6] == "hyper-":
+                self.links[tag]()
+                return
+
+
+class ScrollableFrameH(Frame):
+    """Class to contain horizontal scrollbar for the devices PC icon's (custom checkbutton)."""
+
+    def __init__(self, container, heightParameter, widthParameter, *args, **kwargs):
+        super().__init__(container, *args, **kwargs)
+        canvas = Canvas(self)
+        canvas.config(height=heightParameter, width=widthParameter)
+        scrollbar = Scrollbar(self, orient="horizontal", command=canvas.xview)
+        self.scrollable_frame = Frame(canvas)
+
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: canvas.configure(
+                scrollregion=canvas.bbox("all")
+            )
+        )
+
+        canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+
+        canvas.configure(xscrollcommand=scrollbar.set)
+
+        canvas.pack(side="top", fill="x", expand=True)
+        scrollbar.pack(side="bottom", fill="both")
+
+
+class VerticalScrolledFrame:
+    """
+    A vertically scrolled Frame that can be treated like any other Frame
+    ie it needs a master and layout and it can be a master.
+    :width:, :height:, :bg: are passed to the underlying Canvas
+    :bg: and all other keyword arguments are passed to the inner Frame
+    note that a widget layed out in this frame will have a self.master 3 layers deep,
+    (outer Frame, Canvas, inner Frame) so
+    if you subclass this there is no built in way for the children to access it.
+    You need to provide the controller separately.
+    """
+
+    def __init__(self, master, **kwargs):
+        width = kwargs.pop('width', None)
+        height = kwargs.pop('height', None)
+        bg = kwargs.pop('bg', kwargs.pop('background', None))
+        self.outer = Frame(master, **kwargs)
+
+        self.vsb = Scrollbar(self.outer, orient=VERTICAL)
+        self.vsb.pack(fill=Y, side=RIGHT)
+        self.canvas = Canvas(self.outer, highlightthickness=0, width=width, height=height, bg=bg)
+        self.canvas.pack(side=LEFT, fill=BOTH, expand=True)
+        self.canvas['yscrollcommand'] = self.vsb.set
+        # mouse scroll does not seem to work with just "bind"; You have
+        # to use "bind_all". Therefore to use multiple windows you have
+        # to bind_all in the current widget
+        self.canvas.bind("<Enter>", self._bind_mouse)
+        self.canvas.bind("<Leave>", self._unbind_mouse)
+        self.vsb['command'] = self.canvas.yview
+
+        self.inner = Frame(self.canvas, bg=bg)
+        # pack the inner Frame into the Canvas with the topleft corner 4 pixels offset
+        self.canvas.create_window(4, 4, window=self.inner, anchor='nw')
+        self.inner.bind("<Configure>", self._on_frame_configure)
+
+        self.outer_attr = set(dir(Widget))
+
+    def __getattr__(self, item):
+        if item in self.outer_attr:
+            # geometry attributes etc (eg pack, destroy, tkraise) are passed on to self.outer
+            return getattr(self.outer, item)
+        else:
+            # all other attributes (_w, children, etc) are passed to self.inner
+            return getattr(self.inner, item)
+
+    def _on_frame_configure(self, event=None):
+        x1, y1, x2, y2 = self.canvas.bbox("all")
+        height = self.canvas.winfo_height()
+        self.canvas.config(scrollregion=(0, 0, x2, max(y2, height)))
+
+    def _bind_mouse(self, event=None):
+        self.canvas.bind_all("<4>", self._on_mousewheel)
+        self.canvas.bind_all("<5>", self._on_mousewheel)
+        self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
+
+    def _unbind_mouse(self, event=None):
+        self.canvas.unbind_all("<4>")
+        self.canvas.unbind_all("<5>")
+        self.canvas.unbind_all("<MouseWheel>")
+
+    def _on_mousewheel(self, event):
+        """Linux uses event.num; Windows / Mac uses event.delta"""
+        if event.num == 4 or event.delta > 0:
+            self.canvas.yview_scroll(-1, "units")
+        elif event.num == 5 or event.delta < 0:
+            self.canvas.yview_scroll(1, "units")
+
+
+class ScrollableTextBox():
+    """This class creates a textbox object that is used a lot in this project"""
+
+    def __init__(self, container):
+        self.xscrollbar = Scrollbar(container, orient=HORIZONTAL)
+        self.xscrollbar.pack(side=BOTTOM, fill=X)
+        # Vertical (y) Scroll Bar
+        self.yscrollbar = Scrollbar(container)
+        self.yscrollbar.pack(side=RIGHT, fill=Y)
+
+        # Text Widget
+        self.textbox = Text(container, wrap=NONE,
+                            xscrollcommand=self.xscrollbar.set,
+                            yscrollcommand=self.yscrollbar.set)
+        self.textbox.pack(fill='both', expand=True)
+
+        # Configure the scrollbars
+        self.xscrollbar.config(command=self.textbox.xview)
+        self.yscrollbar.config(command=self.textbox.yview)
